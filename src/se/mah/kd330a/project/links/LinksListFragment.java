ff713a0c422bbce97463504aa6a74c38d288baf1
Isak <Isak@isaks-dator>
2014-05-12 12:05:26 +0200

    test

--- src/se/mah/kd330a/project/links/LinksListFragment.java
+++ src/se/mah/kd330a/project/links/LinksListFragment.java
@@ -16,7 +16,7 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Toast;
 
 //my new comment (isaks edition)
-//new comment
+// new comment from isak
 
 public class LinksListFragment extends Fragment {
     
