d2a586d8a16a9d83bca9653238cc57c84a86c15a
Anton Nyman <anton@antonnyman.se>
2014-05-14 11:52:53 +0200

    Added MAH News.

--- src/se/mah/kd330a/project/adladok/model/Refresh.java
+++ src/se/mah/kd330a/project/adladok/model/Refresh.java
@@ -1,9 +1,16 @@
 package se.mah.kd330a.project.adladok.model;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.URL;
 
 import se.mah.kd330a.project.adladok.model.ScheduleFixedDelay.UpdateType;
 import se.mah.kd330a.project.adladok.xmlparser.Parser;
+import se.mah.kd330a.project.home.data.DOMParser;
+import se.mah.kd330a.project.home.data.RSSFeed;
 import se.mah.kd330a.project.schedule.data.KronoxCalendar;
 import se.mah.kd330a.project.schedule.data.KronoxReader;
 import android.content.Context;
@@ -28,6 +35,11 @@ public class Refresh extends AsyncTask<Void, Void, Void> {
 	@Override
 	protected Void doInBackground(Void... params) {
 		
+		/**
+		 * Fetches user info by sending them to a web service.
+		 * TODO: This is a huge security risk, since the users credentials are  sent in plaintext. Consider using OAuth or obfuscating the password.
+		 */
+		
 		try{
     		String userInfoAsXML = Me.getInstance().getUserInfoAsXML(Me.getInstance().getUserID(),Me.getInstance().getPassword());
     		if(!userInfoAsXML.isEmpty()&&Parser.updateMeFromADandLADOK(userInfoAsXML, mContext)){
@@ -47,11 +59,27 @@ public class Refresh extends AsyncTask<Void, Void, Void> {
 			KronoxCalendar.createCalendar(KronoxReader.getFile(mContext.getApplicationContext()));
 			Log.d("Async", "Downloading...");
 		} catch (Exception e) {
-			Log.d("Async", e.toString());
+			e.printStackTrace();
 		}
 		
 		
+		/**
+		 * Update MAH News feed and parse to xml
+		 */
 		
+		try {
+			DOMParser domParser = new DOMParser();
+			RSSFeed rssFeed = domParser.parseXml(Constants.mahNewsAdress);
+			FileOutputStream fout = mContext.openFileOutput(Constants.mahNewsSavedFileName, Context.MODE_PRIVATE);
+			ObjectOutputStream out = new ObjectOutputStream(fout);
+			out.writeObject(rssFeed);
+			out.close();
+			fout.close();
+			Log.d("Async", "Saved MAH News");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		return null;
 	}
