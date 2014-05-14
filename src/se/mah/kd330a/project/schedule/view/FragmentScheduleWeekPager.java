daa84ce43cc34b301b40e786400a1a82616b464c
Anton <anton@antonnyman.se>
2014-05-14 08:55:43 +0200

    Updated support library. Created a new file called Retrieve wich will support pull to refresh.

--- src/se/mah/kd330a/project/schedule/view/FragmentScheduleWeekPager.java
+++ src/se/mah/kd330a/project/schedule/view/FragmentScheduleWeekPager.java
@@ -11,22 +11,28 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class FragmentScheduleWeekPager extends Fragment {
+public class FragmentScheduleWeekPager extends Fragment implements OnRefreshListener {
 
 	private static ArrayList<ScheduleWeek> myScheduleInWeeks;
 	private ParseData parseData;
 	private static int numItems = 0;
 
+	private SwipeRefreshLayout swipeRefreshLayout;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		parseData = new ParseData();
 		myScheduleInWeeks = parseData.getParsedDataFromKronoxByWeekNew(20);
+		
+		swipeRefreshLayout = (SwipeRefreshLayout) getActivity().findViewById(R.id.swipe_container);
+		
 		if (myScheduleInWeeks != null) {
 			numItems = myScheduleInWeeks.size();
 			Log.i("onCreate in FragmentScheduleWeekPager",Integer.toString(numItems));
@@ -82,6 +88,54 @@ public class FragmentScheduleWeekPager extends Fragment {
 			return numItems;
 		}
 		
+		
+	}
+	
+    private void initSwipeOptions() {
+        swipeRefreshLayout.setOnRefreshListener(this);
+        setAppearance();
+        disableSwipe();
+    }
+ 
+    private void setAppearance() {
+        swipeRefreshLayout.setColorScheme(android.R.color.holo_blue_bright,
+        android.R.color.holo_green_light,
+        android.R.color.holo_orange_light,
+        android.R.color.holo_red_light);
+    }
+ 
+    /**
+     * It shows the SwipeRefreshLayout progress
+     */
+    public void showSwipeProgress() {
+        swipeRefreshLayout.setRefreshing(true);
+    }
+ 
+    /**
+     * It shows the SwipeRefreshLayout progress
+     */
+    public void hideSwipeProgress() {
+        swipeRefreshLayout.setRefreshing(false);
+    }
+ 
+    /**
+     * Enables swipe gesture
+     */
+    public void enableSwipe() {
+        swipeRefreshLayout.setEnabled(true);
+    }
+ 
+    /**
+     * Disables swipe gesture. It prevents manual gestures but keeps the option tu show
+     * refreshing programatically.
+     */
+    public void disableSwipe() {
+        swipeRefreshLayout.setEnabled(false);
+    }
+
+	@Override
+	public void onRefresh() {
+		// TODO Auto-generated method stub
 		
 	}
 	
