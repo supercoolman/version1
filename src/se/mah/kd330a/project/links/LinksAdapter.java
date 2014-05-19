cb1471726383b76728d93e39a226de45807f0199
Isak <Isak@isaks-dator>
2014-05-19 13:15:21 +0200

    phone stuff

--- src/se/mah/kd330a/project/links/LinksAdapter.java
+++ src/se/mah/kd330a/project/links/LinksAdapter.java
@@ -1,9 +1,13 @@
 package se.mah.kd330a.project.links;
 
 import se.mah.kd330a.project.R;
+import se.mah.kd330a.project.StartActivity;
 import android.R.anim;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,6 +22,7 @@ public class LinksAdapter extends ArrayAdapter<String> {
     Activity context;
     String[] linkTitle;
     int viewPagerPosition;
+    String phone = "0709704828";
 
     public LinksAdapter(Activity context, int resource, String[] linkTitle) {
         super(context, resource, linkTitle);
@@ -27,27 +32,28 @@ public class LinksAdapter extends ArrayAdapter<String> {
     
     
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int pos, View convertView, ViewGroup parent) {
         View row = convertView;
-        
         LayoutInflater inflater = context.getLayoutInflater();
         row = inflater.inflate(R.layout.link_list_item, null);
-        viewPagerPosition = LinksChildFragment.position;
-        if(viewPagerPosition == 1) {
-            ImageButton imageButton = (ImageButton) row.findViewById(R.id.image_button);
-            if(position == 0) {
-                imageButton.setBackgroundResource(R.drawable.imagebutton_states);
-                imageButton.setImageResource(R.drawable.ic_action_call);
+        viewPagerPosition = LinksParentFragment.viewPager.getCurrentItem();
+        
+        ImageButton imageButton = (ImageButton) row.findViewById(R.id.image_button);
+        imageButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                startDialActivity(phone);
             }
-            imageButton.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View arg0) {
-                    Toast.makeText(context, "making call", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
+        });
+        
         TextView textView_1 = (TextView) row.findViewById(R.id.title);
-        textView_1.setText(linkTitle[position]);
+        textView_1.setText(linkTitle[pos]);
         return row;
+    }
+    
+    private void startDialActivity(String phone){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:"+phone));
+        context.startActivity(intent);
     }
 }
